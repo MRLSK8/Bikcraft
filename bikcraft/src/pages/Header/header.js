fc520e64c10d648d90f57e9cf398378100618350
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-12-28 11:20:35 -0300

    Onganizando o código do header.

--- bikcraft/src/pages/Header/header.js
+++ bikcraft/src/pages/Header/header.js
@@ -7,17 +7,39 @@ export default function Header() {
   return (
     <header className='header'>
       <div className='container'>
-          <nav className='nav-links'>
-            <Link to='/'> <img src={Logo} alt='logo' /> </Link>
-            <ul>
-              <li><Link to='/'> <span>Home</span></Link></li>
-              <li><Link to='/about'><span>About</span></Link></li>
-              <li><Link to='/product'><span>Products</span></Link></li>
-              <li><Link to='/portifolio'><span>Portfolio</span></Link></li>
-              <li><Link to='/contact-us'><span>Contact us</span></Link></li>
-            </ul>
-          </nav>
-        </div>
+        <nav className='nav-links'>
+          <Link to='/'>
+            <img src={Logo} alt='logo' />
+          </Link>
+          <ul>
+            <li>
+              <Link to='/'>
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to='/about'>
+                <span>About</span>
+              </Link>
+            </li>
+            <li>
+              <Link to='/product'>
+                <span>Products</span>
+              </Link>
+            </li>
+            <li>
+              <Link to='/portifolio'>
+                <span>Portfolio</span>
+              </Link>
+            </li>
+            <li>
+              <Link to='/contact-us'>
+                <span>Contact us</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
